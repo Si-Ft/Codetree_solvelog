edf553a7a2d3359d6504c32b3b5786fce256c6f1
Si-Ft <62862468+Si-Ft@users.noreply.github.com>
2026-09-07 14:32:39 +0900

    [Passed] 동전이 있는 위치 (67ms, 16MB)

---
+++ trail1/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,7 @@
+N,M=map(int,input().split())
+arr=[[0 for _ in range(N)] for _ in range(N)]
+for _ in range(M):
+    y,x=map(int,input().split())
+    arr[y-1][x-1]=1
+for i in range(N):
+    print(*arr[i])
