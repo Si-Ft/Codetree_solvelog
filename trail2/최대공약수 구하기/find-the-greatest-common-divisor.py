95f63344d746eea8be2fc7fabaa155337478a070
Si-Ft <62862468+Si-Ft@users.noreply.github.com>
2026-09-08 10:27:58 +0900

    [Passed] 최대공약수 구하기 (69ms, 16MB)

---
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def gcd(n, m):
+    for i in range(min(n,m), 0, -1):
+        if n%i==0 and m%i==0:
+            return i
+print(gcd(n,m))
