ae752003104dcbc58f04eec4dfd9c3220348d15c
Si-Ft <62862468+Si-Ft@users.noreply.github.com>
2026-09-08 17:50:05 +0900

    [Passed] 두 정수에 대한 연산값 2 (49ms, 16MB)

---
+++ trail2/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def ret_val(a,b):
+    if a>b:
+        a*=2
+        b+=10
+    else:
+        a+=10
+        b*=2
+    return a,b
+print(*ret_val(a,b))
