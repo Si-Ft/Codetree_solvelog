742dc5dcf5e400422c8403f18707ed9dde3cb09f
Si-Ft <62862468+Si-Ft@users.noreply.github.com>
2026-09-09 12:00:59 +0900

    [Passed] 007 (50ms, 16MB)

---
+++ trail2/007/007.py
@@ -0,0 +1,9 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+print(
+    f"""secret code : {secret_code}
+meeting point : {meeting_point}
+time : {time}"""
+)
