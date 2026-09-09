f3216e3cd98fe18d243880eb5ed6c0c1ce186f51
Si-Ft <62862468+Si-Ft@users.noreply.github.com>
2026-09-09 14:24:36 +0900

    [Passed] Date to Date (52ms, 16MB)

---
+++ trail2/Date to Date/date-to-date.py
@@ -0,0 +1,7 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+st = sum(days[:m1])+d1
+en = sum(days[:m2])+d2
+print(en-st+1)
